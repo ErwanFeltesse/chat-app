5cae46a51ddc0ba42c7d560fcb4c319a031f41e2
wilder <er.feltesse@gmail.com>
2020-04-01 10:57:51 +0200

    ajout du protocol propTypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,27 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 
 const Contact = (props) => {
-const {name, avatar, online} = props;
+const {name, avatar, online} = props; 
 return(
 <div className="Contact">
-   <img className="avatar" src={avatar}/>
+   <img className="avatar" alt="avatar" src={avatar}/>
    <div>
        <h3 className="name">{name}</h3>
            <div className="status"> 
-              <span className={props.online ? 'status-online' : 'status-offline'}></span>
-               <p className="status-text"> {online == true? 'online':'offline'} </p> 
+              <span className={props.online ? 'status-online' : 'status-offline'}></span> 
+               <p className="status-text"> {online === true? 'online':'offline'} </p> 
           </div>
   </div>
 </div>
 );
 } 
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool
+}
+
 
 export default Contact;
