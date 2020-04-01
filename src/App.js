bda5746d2493df2801a3e48d94e3f67106e33dd0
wilder <er.feltesse@gmail.com>
2020-04-01 18:39:16 +0200

    affichage list terminée

--- src/App.js
+++ src/App.js
@@ -7,21 +7,7 @@ import './components/Contactstyle.css';
 function App() {
   return (
     <div className="App">
-  <Contact
-  
-  avatar="https://randomuser.me/api/portraits/men/58.jpg"
-  name = "Mathew Spencer"
-  online = {true}
-   />
-   <Contact
-   avatar = "https://randomuser.me/api/portraits/men/13.jpg"
-   name = "Alvin Lawrence"
-   online = {false}/>
-   <Contact
-   avatar ="https://randomuser.me/api/portraits/men/93.jpg"
-   name = "Cameron Pierce"
-   online ={true}/>
-
+    <Contact/>
   </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import users from "./ContactList"
 
+const Contact = () => (
 
-const Contact = (props) => {
-const {name, avatar, online} = props; 
-return(
-<div className="Contact">
-   <img className="avatar" alt="avatar" src={avatar}/>
+<ul>
+  {users.map(user=> (
+<div className="Contact" key = {user.name}>
+   <img className="avatar" alt="avatar" src={user.avatar}/>
    <div>
-       <h3 className="name">{name}</h3>
+       <h3 className="name">{user.name}</h3>
            <div className="status"> 
-              <span className={props.online ? 'status-online' : 'status-offline'}></span> 
-               <p className="status-text"> {online === true? 'online':'offline'} </p> 
+              <span className={user.online ? 'status-online' : 'status-offline'}></span> 
+               <p className="status-text"> {user.online === true? 'online':'offline'} </p> 
           </div>
   </div>
 </div>
-);
-} 
-Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  online: PropTypes.bool
-}
 
+))
+} 
+</ul>
+)
 
 export default Contact;
